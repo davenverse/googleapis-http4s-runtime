824c2f9907c3e022337b660e9aed8c50f590e99c
i10416 <ito.yo16uh90616@gmail.com>
2024-01-04 05:25:11 +0900

    tidy: address linter warnings

--- runtime/shared/src/main/scala/org/http4s/googleapis/runtime/auth/OAuth2.scala
+++ runtime/shared/src/main/scala/org/http4s/googleapis/runtime/auth/OAuth2.scala
@@ -17,14 +17,7 @@
 package org.http4s
 package googleapis.runtime.auth
 
-import cats.data.EitherT
-import cats.effect.kernel.Temporal
-import cats.syntax.all._
-import io.circe.Decoder
-import org.http4s.circe.jsonOf
 import scodec.bits.ByteVector
-
-import scala.concurrent.duration._
 
 trait OAuth2[F[_]] {
   def getAccessToken(
